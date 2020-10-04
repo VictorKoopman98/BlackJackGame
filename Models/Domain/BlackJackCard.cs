7fcc27053e3766494b7901f8eb3fcbfea7be19cc
Victor Koopman <victor.koopman@student.hogent.be>
2020-10-04 16:33:22 +0200

    card and blackjackcard

---
+++ Models/Domain/BlackJackCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models.Domain
+{
+    public class BlackJackCard : Card
+    {
+        public bool FaceUp { get; private set; }
+        public int Value { get; }
+        public BlackJackCard(FaceValue faceValue, Suit suit) : base(faceValue, suit)
+        {
+            FaceUp = false;
+        }
+        public void TurnCard()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Domain/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models.Domain
+{
+    class Card
+    {
+        public FaceValue FaceValue { get; }
+        public Suit Suit { get;  }
+        public Card (FaceValue faceValue, Suit suit)
+        {
+            FaceValue = faceValue;
+            Suit = suit;
+        }
+    }
+}
